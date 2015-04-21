70c4c9c974730f986885da98f06c162ad6c77037
Searoose <andre.sjoroos@gmail.com>
2015-04-21 15:24:40 +0200

    added check registering user email

---
+++ Python och templates/Functions for nabo.py
@@ -0,0 +1,91 @@
+@route("/myProfile/", method="POST")
+def register_user():
+	"""Register user and saves the name, surename, adress, email and
+	password in a document"""
+	global username, email
+
+	contact = []
+	name = request.forms.name
+	surname = request.forms.surname
+	adress = request.forms.adress
+	email = request.forms.email
+	pwd_1 = request.forms.pwd_1
+
+	contact.extend((name, surname, adress, pwd_1))
+        
+	mail_doc = email + ".txt"
+	f = open("user/" + email + ".txt", "r")
+	
+        if mail_doc == f:
+            return "User already exist"
+        else:
+	text_file = open("user/" + email + ".txt", "w")
+
+	for i in contact:
+		text_file.write(i)
+		text_file.write("\n")
+	text_file.close()
+
+	f = open("user/" + email + ".txt", "r")
+	text_file = f.readlines()
+	firstname = text_file[0]
+	surname = text_file[1]
+	username = firstname + surname
+	f.close()
+
+	return template("myProfile", title=email, text=contact, username=username)
+
+
+
+@route("/editProfile/")
+def edit_profile():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("editProfile", username=username)
+    
+
+@route("/inbox/")
+def inbox():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("inbox", username=username)
+
+
+@route("/help/")
+def nabo_help():
+	"""
+	This is the nabo help.
+	"""
+	global username
+	
+	return template("help", username=username)
+    
+
+@route("/contact/")
+def contact_us():
+	"""
+	This is the contact site where you can find how to contact us.
+	"""
+	global username
+	
+	return template("contact", username=username)
+
+
+@route("/about/")
+def about_us():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("about", username=username)
+
+
+
+
--- Python och templates/Nabo.py
+++ Python och templates/Nabo.py
@@ -18,23 +18,6 @@ def start():
 	"""      
 	return template("index")
 
-@route("/nabos/")
-def nabolist():
-	"""Appends usernames into name_list and prints it on the nabopage """
-        global username
-        
-        user_list = listdir("user")
-        name_list = []
-
-        for user in user_list:
-                f = open("user/" + user, "r")
-                text_file = f.readlines()
-                firstname = text_file[0]
-                surname = text_file[1]
-                nabos = firstname + surname
-                name_list.append(nabos)
-	return template("nabos", user_list=user_list, name_list=name_list, username=username)
-
 
 @route("/myProfile/", method="POST")
 def register_user():
@@ -51,21 +34,28 @@ def register_user():
 
 	contact.extend((name, surname, adress, pwd_1))
 
-	text_file = open("user/" + email + ".txt", "w")
+        mail = email + ".txt"
+        
+        if mail in listdir("user"):
+                
+                return "user already exist"
+        
+        else:
+                text_file = open("user/" + email + ".txt", "w")
+                
+                for i in contact:
+                        text_file.write(i)
+                        text_file.write("\n")
+                text_file.close()
 
-	for i in contact:
-		text_file.write(i)
-		text_file.write("\n")
-	text_file.close()
+                f = open("user/" + email + ".txt", "r")
+                text_file = f.readlines()
+                firstname = text_file[0]
+                surname = text_file[1]
+                username = firstname + surname
+                f.close()
+                return template("myProfile", title=email, text=contact, firstname=firstname, username=username)
 
-	f = open("user/" + email + ".txt", "r")
-	text_file = f.readlines()
-	firstname = text_file[0]
-	surname = text_file[1]
-	username = firstname + surname
-	f.close()
-
-	return template("myProfile", title=email, text=contact, username=username)
 
 @route("/myProfile/")
 def register_user():
@@ -105,6 +95,73 @@ def sign_in():
 def my_profile():
         global username, email
 
-        return template("home", username=username) 
+        return template("home", username=username)
+
+@route("/nabos/")
+def nabolist():
+	"""Appends usernames into name_list and prints it on the nabopage """
+        global username
+        
+        user_list = listdir("user")
+        name_list = []
+
+        for user in user_list:
+                f = open("user/" + user, "r")
+                text_file = f.readlines()
+                firstname = text_file[0]
+                surname = text_file[1]
+                nabos = firstname + surname
+                name_list.append(nabos)
+	return template("nabos", user_list=user_list, name_list=name_list, username=username)
+
+
+@route("/editProfile/")
+def edit_profile():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("editProfile", username=username)
+    
+
+@route("/inbox/")
+def inbox():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("inbox", username=username)
+
+
+@route("/help/")
+def nabo_help():
+	"""
+	This is the nabo help.
+	"""
+	global username
+	
+	return template("help", username=username)
+    
+
+@route("/contact/")
+def contact_us():
+	"""
+	This is the contact site where you can find how to contact us.
+	"""
+	global username
+	
+	return template("contact", username=username)
+
+
+@route("/about/")
+def about_us():
+	"""
+	This is the mail inbox
+	"""
+	global username
+	
+	return template("about", username=username)
 
 run(host='localhost', port=8080, debug=True, reloader=True)
